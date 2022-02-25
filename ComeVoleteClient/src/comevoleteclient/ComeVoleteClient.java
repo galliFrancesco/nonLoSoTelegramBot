b7965989abf1edbc5533c239a1dd344536bc5e2b
JEANMONNET\galli_francesco <galifrancesco.2809@gmail.com>
2022-02-25 13:57:11 +0100

    Come Voelte

---
+++ ComeVoleteClient/src/comevoleteclient/ComeVoleteClient.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comevoleteclient;
+
+import TelegramAPI.*;   // questo è il pacchetto della libreria invece
+/**
+ *
+ * @author galli_francesco
+ */
+public class ComeVoleteClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // test è il file della libreria
+        TEST t = new TEST();
+        
+        t.foo(); 
+    }
+    
+}
