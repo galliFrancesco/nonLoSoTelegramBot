6b96f35ed980f9dce4e4eb43c60d691b9bbb48a1
galliFrancesco <galifranceosco.2809@gmail.com>
2022-03-24 22:55:44 +0100

    Migliorie nel file per gestiore le APi tg

--- BotTelegramAPI/src/TelegramAPI/Test.java
+++ BotTelegramAPI/src/TelegramAPI/Test.java
@@ -9,45 +9,44 @@ import java.util.stream.Collectors;
 import org.json.*;
 
 /**
- *
+ * @brief Gestire le API telegram i guess
  * @author galli_francesco
  */
 public class Test {
+
     String URLBase;
-    
-    
+
     String testo;
     Long id_chat, id_message;
-    
 
     public Test() {
         URLBase = "https://api.telegram.org/bot5166569514:AAFfr4N1FeRgUIMPzI3HIKaNNbkzWAaDRqw/"; // url del mio bot
         // totally not my token, take IT... it's public
     }
-    
+
     // metodo per leggere i messaggi dell'utente
     public void getUpdates(String azione) throws MalformedURLException, IOException {
-        
+
         String getUpdatesRequest = URLBase + azione;
         URL request = new URL(getUpdatesRequest); // fa la richiesta web
-        
+
         String result = new BufferedReader(new InputStreamReader(request.openStream())).lines().collect(Collectors.joining("\n"));
         //System.out.println(result);
         String jsonString = result; //assign your JSON String here
         JSONObject obj = new JSONObject(jsonString);
         JSONArray arrResult = obj.getJSONArray("result"); // notice that `"posts": [...]`
-        
+
         // SOUT -> Messaggio /n NoomeUtente
         for (int i = 0; i < arrResult.length(); i++) {
             //ho il totale
             JSONObject totale = arrResult.getJSONObject(i);
-            
+
             //entro nel messaggio
             JSONObject message = totale.getJSONObject("message");
             id_message = message.getLong("message_id");
             testo = message.getString("text");
             //System.out.println(message);
-            
+
             //entro nel from(CHI ha mandato il messaggio)
             JSONObject from = message.getJSONObject("from");
             id_chat = from.getLong("id");
@@ -59,7 +58,7 @@ public class Test {
 
     //questo è per inviare i messaggi
     public void sendMessage(String azione) throws MalformedURLException, IOException {
-        
+
         String getUpdatesRequest = URLBase + azione;
         URL request = new URL(getUpdatesRequest);
 
@@ -68,16 +67,13 @@ public class Test {
         JSONObject obj = new JSONObject(jsonString);
     }
 
-    public String getTesto(){
+    public String getTesto() {
         return testo;
     }
-    
-    public Long getIdChat(){
-        return id_chat; 
+    public Long getIdChat() {
+        return id_chat;
     }
-    
-    public Long getIdMessaggio(){
-        return id_message; 
+    public Long getIdMessaggio() {
+        return id_message;
     }
-
 }
