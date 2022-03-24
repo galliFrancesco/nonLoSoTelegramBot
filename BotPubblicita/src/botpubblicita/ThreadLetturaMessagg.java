e741033ef125558ade3e3460ec5024e552ee5e78
galliFrancesco <galifranceosco.2809@gmail.com>
2022-03-24 23:46:46 +0100

    Migliorie

--- BotPubblicita/src/botpubblicita/ThreadLetturaMessagg.java
+++ BotPubblicita/src/botpubblicita/ThreadLetturaMessagg.java
@@ -2,8 +2,11 @@ package botpubblicita;
 
 import TelegramAPI.Test;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
 /**
  * @author galliFrancesco
@@ -11,6 +14,9 @@ import java.util.logging.Logger;
  */
 public class ThreadLetturaMessagg extends Thread {
 
+    openMap map = new openMap(); 
+    
+    
     Test t = new Test();
 
     Long id_chat;
@@ -37,14 +43,17 @@ public class ThreadLetturaMessagg extends Thread {
                 Logger.getLogger(ThreadLetturaMessagg.class.getName()).log(Level.SEVERE, null, ex);
             }
             // Ok legge i messaggi, 
-            System.out.println(testo);
+            System.out.println(id_chat + ": " + testo);
 
             //if(!testoPrec.equals(testo)){}  // Se non è uguale a quello di adesso 
             // richiama il metodo per dire che c'è un messaggio diverso       
             
             if (id_messaggio != id_messaggio2) { //Se sono uguali, allora non si hanno inviato nuovi messaggi 
+                
                 // MEGLIO INCLUDERE ANCHE L'ID DEL MESSAGGIO -> se è lo stesso, non fa le cose 
-
+                
+                id_messaggio2 = id_messaggio; // così dovrebbe farlo una sola volta
+                
                 // Varie possibilità di messaggi 
                 // Saluto(Era per testare, ma può essere un easter Egg)
                 if (testo.equals("Ciao")) {
@@ -58,18 +67,33 @@ public class ThreadLetturaMessagg extends Thread {
                 // Impostazione della città
                 if (testo.equals("/citta")) {
                     try {
-                        
+
                         // qui c'è da richiamare la cosa che fa con OpenMap
                         t.sendMessage("sendMessage?chat_id=" + id_chat + "&text=Dove abiti?");
+                        t.sendMessage("sendMessage?chat_id=" + id_chat + "&text=(ex. Longone al Segrino, Como)");
+
+                        // qui dovrebbe prendere poi il messaggio, quindi aspettare finchè l'id non è diverso 
+                        do {
+                            // Cambierà il messaggio               
+                            t.getUpdates("getUpdates");
+                            testo = t.getTesto();
+                            id_messaggio = t.getIdMessaggio();            
+                            
+                        } while (id_messaggio == id_messaggio2); // Aspetta il messaggio dopo
                         
+                            // che dovrebbe generare la query
+                            // che per qualche stracazzo di motivo è vuota
+                            testo = testo.replaceAll(" ","+"); // FUNZIA :D
+                        
+
+
                     } catch (IOException ex) {
                         Logger.getLogger(ThreadLetturaMessagg.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                } 
-              
-                id_messaggio2 = id_messaggio; // così dovrebbe farlo una sola volta
-                
-            } 
+                }
+
+            }
+            
             // "Addormenta" il Thread, altrimenti farebbe troppi update
             try {
                 Thread.sleep(2000);
@@ -78,5 +102,6 @@ public class ThreadLetturaMessagg extends Thread {
             }
 
         } while (true);
+        }
     }
-}
+   
